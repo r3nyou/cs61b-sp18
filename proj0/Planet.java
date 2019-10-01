bdd555964d4374bf3ace4c89d59ef47872fcb10b
r3nyou <renyou815@gmail.com>
2019-10-01 23:13:44 +0800

    modify G from public to private

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -16,7 +16,7 @@ public class Planet {
     /** The name of the file that corresponds to the image that depicts the body */
     public String imgFileName;
 
-    public static final double G = 6.67e-11;
+    private static final double G = 6.67e-11;
 
     public Planet(double xP, double yP, double xV, double yV, double m, String img) {
         xxPos = xP;
