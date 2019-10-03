7a59c05c90095242832c3d5d149956f6bcc30946
r3nyou <renyou815@gmail.com>
2019-10-03 21:53:36 +0800

    fix dcatenate input A could be null

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,7 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        IntList p = A.rest;
+        IntList p = new IntList(0, A);
 
         while (p.rest != null) {
             p = p.rest;
